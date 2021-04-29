8b37257ab8bbd341b13d9579a8d992e25dac0b5b
HEEJU-a <81272648+HEEJU-a@users.noreply.github.com>
2021-04-29 16:53:28 +0900

    Add files via upload

---
+++ 4월 29일.sql
@@ -0,0 +1,13 @@
+CREATE TABLE t_board(
+ iboard INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+ title VARCHAR(100) NOT NULL,
+ ctnt VARCHAR(500) NOT NULL,
+ regdt DATETIME DEFAULT NOW()
+);
+
+SELECT * FROM t_board;
+
+INSERT INTO t_board
+(title, ctnt)
+VALUES
+(' 안녕','하하');
